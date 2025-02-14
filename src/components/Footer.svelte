cb49ed5d142469c99685470ffc8324716cf4b2e1
Ronald Chen <ronaldchen7.12@gmail.com>
2025-02-13 21:36:07 -0500

    fix LN

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,11 @@
 <div class="bg-cover bg-[url('/bg/footer.png')]">
     <img class="px-12 pt-8 h-auto max-w-full lg:px-20" src="./bg/footer-text.png" alt>
     <div class="text-white px-16 py-16 lg:pt-24 lg:text-lg lg:flex lg:justify-between">
-        <div class="pt-2">2024 · Zeaj Zamoranos</div>
+        <div class="pt-2">2025 · Zeaj Zamoranos</div>
         <div class="pt-2">Made with lots of love and matcha 🍵</div>
         <div class="pt-2">
-            <a class="underline pr-2" href>LinkedIn</a>
-            <a class="underline pr-2" href>Email</a>
+            <a class="underline pr-2" href="https://www.linkedin.com/in/zeajzamoranos/" target="_blank">LinkedIn</a>
+            <a class="underline pr-2" href="mailto:zzamoran@uwaterloo.ca">Email</a>
             <a class="underline pr-2" href>Resume</a>
         </div>
     </div>
